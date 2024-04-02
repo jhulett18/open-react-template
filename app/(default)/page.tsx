58b793e0a2dee35b70b3dfac40425e697ba27909
jhulett18 <jonathan_hulett@ymail.com>
2024-04-02 09:48:33 -0400

    naming changes

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'Evolve Hire',
+  description: 'Marketing To Evolve',
 }
 
 import Hero from '@/components/hero'
